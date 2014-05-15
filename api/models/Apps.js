1e24a8e5caa7706936bd182e46ed7fe2f6315e73
urielaero <aero.uriel@gmail.com>
2014-05-15 00:11:30 -0500

    model apps

---
+++ api/models/Apps.js
@@ -0,0 +1,17 @@
+/**
+* Apps.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+	attributes: {
+		name:'string'
+		,controller:'string'
+		,description:'string'
+		,icon:'string'
+	}
+};
+
