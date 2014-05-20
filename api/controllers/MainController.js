5ea006e8fd773c2726360f3c85bcc14212104986
urielaero <aero.uriel@gmail.com>
2014-05-20 11:47:42 -0500

    main controller

---
+++ api/controllers/MainController.js
@@ -0,0 +1,13 @@
+/**
+ * MainController.js 
+ *
+ * @description ::
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+	select_company: function(req,res){
+		req.session.select_company = req.params.id;
+		res.redirect(req.headers.referer || '/home/');
+	}
+};
