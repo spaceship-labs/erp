e0ac0e9e3f9e0e01ab676dc759c0f69b4e1e7461
urielaero <aero.uriel@gmail.com>
2014-05-28 12:17:24 -0500

    fix user permission

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -10,7 +10,8 @@
 module.exports = function(req, res, next) {
 
 	if(req.isAuthenticated()){
-		var company = req.user.companies[req.user.select_company];
+		var select_company = req.session.select_company || req.user.select_company
+		, company = req.user.companies[select_company];
 		if(company && company.indexOf(req.options.controller)!=-1){
 			return next();
 		}
