fefa17eda8c0042b403b551bc797dc397ecb7cf3
root <root@erick.spaceship>
2014-09-12 18:33:06 -0500

    yellow test

--- config/template_config.js
+++ config/template_config.js
@@ -10,4 +10,9 @@ module.exports.template_config = {
 	//Caribe Maya
 	cssFile : 'style-caribe-maya.css',
 	logo : 'caribe-maya.png',
+
+	//Yellowtransfers
+	cssFile : 'style-yellow.css',
+	logo : 'yellow_logo.png',
+	logo2 : 'yellow_logo2.png',
 }
