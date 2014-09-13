7464c06b9b8640b726d895977b4b9c2940554f69
jafet gonzalez <detoxikaided@gmail.com>
2014-09-12 19:53:13 -0500

    projects jquery

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -34,8 +34,8 @@ var jsFilesToInject = [
 	// are brought in here
 	'js/dependencies/**/*.js',
 	'/bower_components/jquery/jquery.min.js',
-	"/bower_components/bootstrap/dist/js/bootstrap.min.js",
     '/bower_components/jquery/dist/jquery.min.js',
+	"/bower_components/bootstrap/dist/js/bootstrap.min.js",
 	//"/bower_components/ng-file-upload/angular-file-upload-shim.js",
 	"/bower_components/angular/angular.min.js",
 	"/bower_components/ng-file-upload/angular-file-upload.min.js",
