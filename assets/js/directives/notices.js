5514ff13a33b37dd63591de504d63a46d8f331bf
el-sonny <sonny@spaceshiplabs.com>
2014-11-03 14:22:13 -0600

    notices

---
+++ assets/js/directives/notices.js
@@ -0,0 +1,20 @@
+(function () {
+	var controller = function($scope,$upload,$http){
+		
+	};
+	controller.$inject = ['$scope','$http'];
+    var directive = function () {
+        return {
+        	controller : controller,
+        	scope : {
+        		object : '=',
+                app : '=',
+                apps : '=',
+
+        	},
+        	templateUrl : '/template/find/notices.html'
+        };
+    };
+    app.directive('notices', directive);
+
+}());
