66e231d0eb874b493ca9e899fc8cbc2025d65670
jafet gonzalez <detoxikaided@gmail.com>
2014-11-04 12:46:42 -0600

    cambios apps para american y creacion de cliente al crear la cotizacion

--- assets/js/controllers/controller.js
+++ assets/js/controllers/controller.js
@@ -292,6 +292,21 @@ app.controller('clientCTL',function($scope){
                 window.location.href = data.url;
         }
     });
+
+    function showResponse(data){
+        if(data){
+            if (data.text)
+                jQuery('.alert p').text(data.text).parent().removeClass('unseen');
+            if(data.url)
+                window.location.href = data.url;
+        }
+    };
+
+    $scope.createClient = function () {
+        //console.log($scope.client_form);
+        var client_form = { name : $scope.client_name,address : $scope.client_address,rfc : $scope.client_rfc , phone : $scope.client_phone };
+        $http.post('/clientes/crear',client_form,{}).success(showResponse);
+    };
 });
 
 app.controller('clientAddCTL',function($scope){
@@ -773,10 +788,21 @@ app.controller('installationCTL',function($scope,$http) {
 });
 
 app.controller('saleAddClientCTL',function($scope,$http){
-    $scope.createClient = function(){
-        $http.post('/client/create_quote');
-    }
+    $scope.createClient = function () {
+        var client_form = { name : $scope.client_name,address : $scope.client_address,rfc : $scope.client_rfc , phone : $scope.client_phone };
+        $http.post('/client/create_quote',client_form,{}).success(showResponse);
+    };
 });
+
+function showResponse(data){
+    console.log(data);
+    if(data){
+        if (data.text)
+            jQuery('.alert p').text(data.text).parent().removeClass('unseen');
+        if(data.url)
+            window.location.href = data.url;
+    }
+};
 
 function updateNotices($scope,url,dt,cb){
 	io.socket.on('update',function(data){
