21cde5d8fe64e3924f989760f3105f64d3acebc0
el-sonny <sonny@spaceshiplabs.com>
2014-12-12 08:53:04 -0600

    tutorial updates

---
+++ api/services/Install.js
@@ -0,0 +1,3 @@
+module.exports.loadCurrencies = function(){
+	
+};
---
+++ config/currencies.js
@@ -0,0 +1,22 @@
+module.exports.currencies = [
+  {
+    "name": "Dollars",
+    "currency_code": "USD",
+    "prefix": "$",
+    "suffix": ""
+  },
+  {
+    
+    "name": "Pesos",
+    "currency_code": "MXN",
+    "prefix": "$",
+    "suffix": "m.n."
+  },
+  {
+    
+    "name": "Euro",
+    "currency_code": "EUR",
+    "prefix": "€",
+    "suffix": ""
+  }
+]
