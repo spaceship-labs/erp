17a5286c689c020c42ff9dcaf0704a802e22592d
el-sonny <sonny@spaceshiplabs.com>
2014-12-18 18:26:55 -0600

    calenard

--- assets/js/directives/calendarMonth.js
+++ assets/js/directives/calendarMonth.js
@@ -1,5 +1,6 @@
 (function () {
 	var controller = function($scope,$http){
+        $scope.monthLabels = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'];
 		//$console.log($scope.month,$scope.year);
 	};
 	controller.$inject = ['$scope','$http'];
@@ -7,8 +8,8 @@
         return {
         	controller : controller,
         	scope : {
-                month : '&',
-                year : '&',
+                month : '=',
+                year : '=',
         	},
         	templateUrl : '/template/find/calendarMonth.html'
         };
