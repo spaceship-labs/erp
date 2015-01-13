0ae14d9cbae6eea251492d3117d7e0c8f91e57bb
el-sonny <sonny@spaceshiplabs.com>
2015-01-13 14:46:58 -0600

    abstract install

--- api/services/Install.js
+++ api/services/Install.js
@@ -1,44 +1,16 @@
-module.exports.loadCurrencies = function(){
-	//console.log('instalando curriencies');
-	_.forEach(sails.config.currencies, function(Currency2) { 
-	//console.log(Currency2);
-		Currency.create(Currency2).exec(function(err,Currency3){
-			if(err){
-				console.log(err);
-				throw err;
-			}
-		});
-
-	});
+module.exports.preload = function(){
+	Install.loadObjects(Currency,sails.config.currencies);
 
 }
 
-module.exports.loadFoodSchemes = function(){
-	//console.log('instalando foodschemes');
-	_.forEach(sails.config.foodschemes, function(foodscheme2) { 
-	//console.log(foodscheme2);
-		FoodScheme.create(foodscheme2).exec(function(err,foodscheme3){
+
+module.exports.loadObjects = function(model,objects){
+	_.forEach(objects, function(object) { 
+		model.create(object).exec(function(err,obj){
 			if(err){
 				console.log(err);
 				throw err;
 			}
 		});
-
 	});
-
-}
-
-module.exports.loadHotelRoomView = function(){
-	console.log('instalando HotelRoomView');
-	_.forEach(sails.config.hotelroomviews, function(HotelRoomView2) { 
-	console.log(HotelRoomView2);
-		HotelRoomView.create(HotelRoomView2).exec(function(err,HotelRoomView3){
-			if(err){
-				console.log(err);
-				throw err;
-			}
-		});
-
-	});
-
 }
