2eca27b97f4ee2f30c2fe47d845580c844c7fecc
el-sonny <sonny@spaceshiplabs.com>
2015-01-16 10:08:54 -0600

    pseudo code async install

--- api/services/Install.js
+++ api/services/Install.js
@@ -1,32 +1,23 @@
 module.exports.preload = function(){
-	Install.loadObjects({model:Currency,objects:sails.config.currencies});
-	Install.loadObjects({model:FoodScheme,objects:sails.config.foodschemes});
-	Install.loadObjects({model:HotelRoomView,objects:sails.config.hotelroomviews});
-	/*Install.loadObjects(Currency,sails.config.currencies);
-	Install.loadObjects(FoodScheme,sails.config.foodschemes);
-	Install.loadObjects(HotelRoomView,sails.config.hotelroomviews);*/
 
+	//Ejecuta la funcion loadObjects en serie;
+	async.series([
+		function(cb){Install.loadObjects({model:Currency,objects:sails.config.currencies})},
+		function(cb){Install.loadObjects({model:FoodScheme,objects:sails.config.foodschemes})},
+		function(cb){Install.loadObjects({model:HotelRoomView,objects:sails.config.hotelroomviews})},
+	],function(e,results){
+		//Esta funcion se llama cuando se termina todo
+		if(e) throw(e);
+		console.log(results);
+	});
 };
 
 
 module.exports.loadObjects = function(params,callback){
+	//Ejecuta findOrCreate asincronamente y manda a llamar el callback cuando termina de procesar todos los objetos
 	async.map(params.objects, (function(object,callback) {
 		params.model.findOrCreate(object, object).exec(function(err,params){
 			console.log(params);
 		});
-	}), function(error, createdOrFoundObjects) {
-		console.log(error, createdOrFoundObjects)
-	})
-
-	/*params.model,params.objects  model.findOrCreate({model:Currency,objects:sails.config.currencies}, object).exec(callback);*/
-	/*_.forEach(objects, function(object) {
-	console.log(model) 
-		model.findOrCreate(object,object).exec(function(err,obj){
-			//console.log(obj)
-			if(err){
-				console.log(err);
-				throw err;
-			}
-		});
-});*/
+	}),callback);
 }
