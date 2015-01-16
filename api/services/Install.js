1b6712d0ba9941481884c0b49094fae76cdd8c74
el-sonny <sonny@spaceshiplabs.com>
2015-01-16 10:21:05 -0600

    install example

--- api/services/Install.js
+++ api/services/Install.js
@@ -10,8 +10,23 @@ module.exports.preload = function(){
 		if(e) throw(e);
 		console.log(results);
 	});
+	
 };
 
+module.exports.preloadAlt = function(){
+	//Otra manera de hacer lo mismo de preload
+	var params = [
+		{model:Currency,objects:sails.config.currencies},
+		{model:FoodScheme,objects:sails.config.foodschemes},
+		{model:HotelRoomView,objects:sails.config.hotelroomviews},
+	];
+
+	async.mapSeries(params,Install.loadObjects,function(e,results){
+		//Esta funcion se llama cuando se termina todo
+		if(e) throw(e);
+		console.log(results);
+	});
+}
 
 module.exports.loadObjects = function(params,callback){
 	//Ejecuta findOrCreate asincronamente y manda a llamar el callback cuando termina de procesar todos los objetos
