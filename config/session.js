e0ce387c8b91c851a1e839b4a5b8e269162da2b9
juanku <juanku@nominal.mx>
2015-04-03 11:22:23 +0000

    persistent redis session

--- config/session.js
+++ config/session.js
@@ -25,6 +25,22 @@ module.exports.session = {
   //   maxAge: 24 * 60 * 60 * 1000  
   // }
   
+  adapter: 'redis',
+
+  /***************************************************************************
+  *                                                                          *
+  * The following values are optional, if no options are set a redis         *
+  * instance running on localhost is expected. Read more about options at:   *
+  * https://github.com/visionmedia/connect-redis                             *
+  *                                                                          *
+  *                                                                          *
+  ***************************************************************************/
+
+  host: 'localhost',
+  port: 6379,
+  db: 0,
+  pass: '',
+  prefix: 'sess:',
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
