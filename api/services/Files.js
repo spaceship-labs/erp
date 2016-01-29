bf5c73bb385cdad6283f971381f5e3854e07d890
Erick <erick@Erick-pc>
2016-01-28 19:11:55 -0500

    crops test

--- api/services/Files.js
+++ api/services/Files.js
@@ -166,7 +166,7 @@ module.exports.makeCropsStreams = function(uploadOptions, opts, cb){
     async.each(sizes,function(size, next){ 
     	var wh = size.split('x');
         gmIm(opts.srcData)
-        //.resize(wh[0], wh[1], '!')
+        .resize(wh[0], wh[1], '^')
         .crop(wh[0], wh[1], 0, 0)
         .stream(function(err, stdout, stderr){
             if(err) return next(err);
